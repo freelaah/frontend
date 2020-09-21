c5bce2de5958811ad112d35cf142474ef40dad12
Moises Guilherme <moisescomp@gmail.com>
2020-09-21 19:09:19 -0300

    add login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from '../pages/Landing';
+import Login from '../pages/Login'
 
 function Routes() {
     return (
         <BrowserRouter>
             <Route path="/" exact component={Landing}/>
+            <Route path="/login" component={Login}/>
         </BrowserRouter>
     )
 }
