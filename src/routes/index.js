d2ef170bcc846e1f86bee6c8a22fa5c273d97154
marcos vinicius <marcosvinicius.udia1256@gmail.com>
2020-09-26 13:59:19 -0300

    Tela de cadastro feita

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from '../pages/Landing';
+import SignUp from '../pages/SignUp';
 
 function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={Landing}/>
+            <Route path="/" exact component={Landing} />
+            <Route path="/cadastrar" exact component={SignUp} />
         </BrowserRouter>
     )
 }
