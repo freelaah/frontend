73e324d9190a4853cdcbc046d081dcfe463d11dd
SillasVinicius <sillasvinicius0403@gmail.com>
2020-09-29 18:34:20 -0300

    finalizando Landing

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,50 +1,114 @@
 import React from 'react';
-
+import {
+FaFacebook,
+FaTwitter,
+FaInstagram,
+FaGithub,
+FaLinkedin,
+FaYoutube,
+} from 'react-icons/fa';
 import { 
   Container,
-  PageLanding,
   LogoContainer,
   Logo,
   LandingImg,
   PageLandingContent,
   PageMore,
+  PageFooter,
   Title,
   ButtonsContainer,
   Service,
   WorkIcon,
   Search,
   SearchIcon,
+  WrapperImage,
+  PageAction,
+  Explication,
+  ImgMore,
+  FtSocial,
+  FtSocialList,
+  FtLegal,
+  FtLegaList,
+  ButtonsContainerMore,
+  Login,
+  LoginIcon,
+  Cadastrar,
+  CadIcon,
 } from './styles';
 
 const Landing = () => {
   return (
     <Container>
-      <PageLanding>
-        <PageLandingContent className="container">
+        <PageLandingContent>
           <LogoContainer>
             <Logo />
             <h2>Conectando quem busca com quem oferece!</h2>
           </LogoContainer>
-          <LandingImg />
+          <WrapperImage>
+            <LandingImg />
+          </WrapperImage>
         </PageLandingContent>
-      </PageLanding>
 
-      <PageMore>
-        <Title>
-          <p>Seja bem-vindo.</p>
-          <p><strong>O que deseja fazer?</strong></p>
-        </Title>
-        <ButtonsContainer>
-          <Service href="/login">
-            <WorkIcon />
-            Prestar Serviço
-          </Service>
-          <Search href="/login">
-            <SearchIcon />
-            Buscar Serviço
-          </Search>
-        </ButtonsContainer>
-      </PageMore>
+        <PageAction>
+          <Title>
+            <p>Seja bem-vindo.</p>
+            <p><strong>O que deseja fazer?</strong></p>
+          </Title>
+          <ButtonsContainer>
+            <Service href="#more">
+              <WorkIcon />
+              Prestar Serviço
+            </Service>
+            <Search href="#more">
+              <SearchIcon />
+              Buscar Serviço
+            </Search>
+          </ButtonsContainer>
+       </PageAction>
+
+       <PageMore>
+          <Explication id="more">
+              <h1>Conectando quem busca com quem oferece!</h1>
+              <p>Conecte com a FreeLaah, não perca tempo!!! Você pode prestar serviço ou buscar serviços. 
+                Entre e confira!! Simples assim.  Faça sua escolha, 
+                clique no botão abaixo, na opção que você desejar! </p>
+              <ButtonsContainerMore>
+                <Login href="/login">
+                  <LoginIcon />
+                  Login           
+                </Login>
+                <Cadastrar href="/login">
+                  <CadIcon />
+                  Cadastrar
+                </Cadastrar>
+              </ButtonsContainerMore>
+          </Explication>
+          <WrapperImage>
+            <ImgMore/>
+          </WrapperImage>
+       </PageMore>
+
+       <PageFooter>
+         <FtSocial>
+           <FtSocialList>
+            <li><a href="#"><FaFacebook /></a></li>
+            <li><a href="#"><FaTwitter /></a></li>
+            <li><a href="#"><FaInstagram /></a></li>
+            <li><a href="#"><FaGithub /></a></li>
+            <li><a href="#"><FaLinkedin /></a></li>
+            <li><a href="#"><FaYoutube /></a></li>
+           </FtSocialList>
+         </FtSocial>
+
+         <FtLegal>
+           <FtLegaList>
+            <li><a href="#">Terms &amp; Conditions</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li>&copy; 2020 Copyright FreeLaah Inc.</li>
+           </FtLegaList>
+         </FtLegal>
+       </PageFooter>
+
     </Container>
   );
 };
--- src/pages/Landing/styles.js
+++ src/pages/Landing/styles.js
@@ -3,42 +3,32 @@ import landing from './../../assets/images/landing.svg';
 import logo from './../../assets/images/logo.svg';
 import workIcon from './../../assets/images/workIcon.svg';
 import searchWorkIcon from './../../assets/images/searchWorkIcon.svg';
-import Login from '../Login';
+import imgMore from './../../assets/images/imgMore.svg';
+import { FaSignInAlt, FaUser } from 'react-icons/fa';
 
 export const Container = styled.div`
-
 `;
 
-export const PageLanding = styled.div`
+// Início - Conteúdo inicial (verde)
+export const PageLandingContent = styled.div`
     width: 100vw;
     height: 430px;
 
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
 
     color: var(--color-text-in-primary);
     background-color: var(--color-primary);
 `;
 
-
-export const PageLandingContent = styled.div`
-    max-width: 1100px;
-
-    display: grid;
-    grid-template-rows: 350px 1fr;
-    grid-template-columns: 2fr 1fr 1fr;
-    grid-template-areas: 
-    "logo hero hero"
-    "buttons buttons total"
-    ;
-`;
-
 export const LogoContainer = styled.div`
-    grid-area: logo;
-    align-self: center;
-    margin: 0;
-    text-align: left;
+    width: 50vw;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
     > h2 {
         font-weight: 500;
@@ -50,28 +40,41 @@ export const LogoContainer = styled.div`
     }
 `;
 
+export const WrapperImage = styled.div`
+   width: 50vw;
+   height: 100%;
+
+   width: 50vw;
+   height: 100%;
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+`;
+
 export const Logo = styled.img.attrs({
     src: logo,
     alt: 'freeLaah'
 })`
-    height: 100%;
 `;
 
 export const LandingImg = styled.img.attrs({
     src: landing,
     alt: 'Plataforma de empregos'
 })`
-    grid-area: hero;
-    justify-content: end;
-    width: 100%;
 `;
 
-export const PageMore = styled.div`
+// Fim - Conteúdo inicial - (verde)
+
+// Inicio - Conteúdo Action - Botões - (Branco)
+export const PageAction = styled.div`
     width: 100vw;
+    height: 205px;
     display: flex; 
     justify-content: space-between; 
     align-items: center;
     padding: 0rem 15rem 0rem 15rem;
+    background: white;
 `;
 
 export const Title = styled.div`
@@ -119,7 +122,7 @@ export const Service = styled.a`
     background: var(--color-primary);
 
     &:hover {
-        background: var(--color-primary-light);
+        background: var(--color-primary-darker);
     }
 `;
 
@@ -150,3 +153,165 @@ export const SearchIcon = styled.img.attrs({
 })`
     ${iconCSS}
 `;
+
+// Fim - Conteúdo Action - Botões - (Branco)
+
+// Início - Conteúdo More - Explicação - (Branco)
+export const PageMore = styled.div`
+    width: 100vw;
+    height: 430px;
+
+    display: flex;
+    align-items: center;
+
+    padding: 0rem 15rem 0rem 15rem;
+`;
+
+export const Explication = styled.div`
+    width: 50vw;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+        font-family: Poppins;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 40px;
+    }
+
+    > p {
+        font-family: Poppins;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+    }
+`;
+
+export const ImgMore = styled.img.attrs({
+    src: imgMore,
+    alt: 'Calendários'
+})`
+`;
+
+export const ButtonsContainerMore = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-top: 3.2rem;
+    width: 100%;
+
+    > a {
+        width: 20rem;
+        height: 7rem;
+        border-radius: 0.8rem;
+        font: 700 2.0rem Archivo;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        text-decoration: none;
+        color: var(--color-button-text);
+
+        transition: background-color 0.2s;
+    }
+`;
+
+export const Login = styled.a`
+    background: var(--color-secondary);
+
+    &:hover {
+        background: var(--color-secondary-dark);
+    }
+`;
+
+export const Cadastrar = styled.a`
+    background: var(--color-primary);
+
+    &:hover {
+        background: var(--color-primary-darker);
+    }
+`;
+
+export const LoginIcon = styled(FaSignInAlt)`
+    ${iconCSS}
+`;
+
+export const CadIcon = styled(FaUser)`
+    ${iconCSS}
+`;
+
+
+// Fim - Conteúdo More - Explicação - (Branco)
+
+// Início - Conteúdo Footer - Botões - (Verde)
+export const PageFooter = styled.div`
+    width: 100vw;
+    line-height: 1.5;
+    background-color: var(--color-primary);
+    color: white;
+`;
+
+export const FtSocial = styled.section`
+    padding: 0 1.875rem 1.25rem;
+`;
+
+export const FtSocialList = styled.ul`
+    display: flex;
+    justify-content: center;
+    padding-top: 1.25rem;
+    list-style: none;
+    padding-left: 0;
+
+    > li {
+        margin: 0.5rem;
+        font-size: 1.25rem;
+
+        > a {
+            text-decoration: none;
+            color: #eee;
+
+            > svg {
+                width: 30px;
+                height: 30px;
+            }
+        }
+    }
+`;
+
+export const FtLegal = styled.section`
+    padding: 0.9375rem 1.875rem;
+    background-color: var(--color-primary-darker);
+`;
+
+export const FtLegaList = styled.ul`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    font-family: Poppins;
+    font-weight: bold;
+
+    > li {
+        margin: 0.125rem 0.625rem;
+        white-space: nowrap;
+        list-style: none;
+        padding: 0rem 15rem 0rem 15rem;
+
+
+        > a {
+            text-decoration: none;
+            color: #eee;
+
+            > svg {
+                width: 30px;
+                height: 30px;
+            }
+        }
+    }
+`;
+// Início - Conteúdo Footer - Botões - (Verde)
