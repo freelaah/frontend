ed27bead112542a78a783a2399d4d2e039a61114
Moises Guilherme <moisescomp@gmail.com>
2020-11-03 21:43:25 -0300

    teste

--- src/pages/Professional/imagem.js
+++ src/pages/Professional/imagem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header';
 
 import api from '../../services/api';
@@ -6,7 +6,9 @@ import api from '../../services/api';
 
 const  ImagemServico = () => {
     
-    let image = null;
+    
+    const [image, setImage] = useState('');
+    var base64 = require('base-64');
 
     openImage();
 
@@ -14,19 +16,19 @@ const  ImagemServico = () => {
         try {
       
             const response = await api.get("/services/5fa1d3853aa8974eee3d15a3");
-            image = response.data.file;
-
-            console.log(">>>>", image)
+            //setImage(`data:image/png;base64,${base64.decode(response.data.file)}`);
+            console.log(">>>>", response.data.baseURL)
+            //setImage(URL.createObjectURL(response.data.baseURL))
        
           }catch(error){
               console.log("erro " + error);
           }     
     }
     
-    
     return (
         <>
             <Header title="Teste de imagem." />
+            
             <img src={image}/>
         </>
         
